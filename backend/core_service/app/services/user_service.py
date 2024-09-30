78c7a251424c76e4eba5de8c754e71ed3ee66e2c
Hamza Goubraim <hamzagoubraim@gmail.com>
2024-10-01 00:21:44 +0100

    Adding User active and not deleted filters

--- backend/core_service/app/services/user_service.py
+++ backend/core_service/app/services/user_service.py
@@ -4,7 +4,12 @@ from dependencies.auth_dependency import bcrypt_context
 
 
 def authenticate_user(user_email:str, user_password:str,db):
-    user =db.query(User).filter(User.user_email==user_email).first()
+    user =db.query(User).filter(
+        User.user_email==user_email,
+        User.is_deleted==False,
+        User.is_active==True,
+        User.status==True
+        ).first()
     if not user:
         return False
     if not bcrypt_context.verify(user_password, user.user_hashed_password):
