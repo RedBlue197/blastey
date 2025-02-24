7a1a33047adcd6fd476bf7f9bcae9786150ba6b4
Hamza Goubraim <hamzagoubraim@gmail.com>
2025-02-24 00:45:09 +0000

    Adding professional dashboard

---
+++ frontend/src/app/professional/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfessionalDashboard from "./professional-dashboard/ProfessionalDashboard"; // Use a CSS module for styles
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <ProfessionalDashboard />
+    </>
+  );
+};
+
+export default Dashboard;
